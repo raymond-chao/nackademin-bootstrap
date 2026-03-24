e595ab1424f7a96b93957c0667341436c6abe799
raymond-chao <raymond_chao45@hotmail.com>
2026-03-24 11:21:50 +0100

    minor changes

--- scripts/items.js
+++ scripts/items.js
@@ -1,4 +1,26 @@
 let cart = [];
+const progressBar = document.getElementById('progress-bar');
+const badge = document.getElementById('badge');
+
+function updateProgressBar() {
+    //top
+    const scrollTop = window.scrollY;
+    //total scroblable height
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const progress = Math.round((scrollTop / docHeight) * 100);
+
+    progressBar.style.width = progress + '%';
+    badge.textContent = progress + '% Läst';
+
+    if (progress >= 100) {
+        badge.classList.add('complete');
+        badge.textContent = 'Complete!';
+    } else {
+        badge.classList.remove('complete');
+    }
+}
+
+window.addEventListener('scroll', updateProgressBar);
 
 function fetchProducts(){
     fetch('https://fakestoreapi.com/products')
